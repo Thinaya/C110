589e913938045d57cc26d00ecfa4e7ab20861da2
Thinaya <81453008+Thinaya@users.noreply.github.com>
2021-11-07 11:38:50 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -16,13 +16,13 @@ function take_snapshot()
     })
 }
 
-classifier = ml5.image_classifier("https://teachablemachine.withgoogle.com/models/BdORE9mGk/model.json",modelLoaded);
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/BdORE9mGk/model.json",modelLoaded);
 
 function speak()
 {
     var Thinaya = window.speechSynthesis;
     speech_1 = "The first prediction is"+prediction_1;
-  speech_2 = "The first prediction is"+prediction_2;
+  speech_2 = "The second prediction is"+prediction_2;
 
   var utterThis=new SpeechSynthesisUtterance(speech_1+speech_2);
   Thinaya.speak(utterThis);
@@ -31,4 +31,47 @@ function speak()
 function modelLoaded()
 {
     console.log("model loaded!");
+}
+
+function check()
+{
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results)
+        document.getElementById("results_gesture_name").innerHTML = results[0].label;
+        document.getElementById("results_gesture_name2").innerHTML = results[1].label;
+      prediction_1 = results[0].label;
+      prediction_2 = results[1].label;
+        speak();
+        if(results[0].label == "Thumbs Up")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128077;";
+        }
+        if(results[0].label == "Thumbs Down")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128078;";
+        }
+        if(results[0].label == "Peace Sign")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#9996;";
+        }
+        if(results[1].label == "Thumbs Up")
+        {
+            document.getElementById("update_emoji").innerHTML = "";
+        }
+        if(results[1].label == "Thumbs Down")
+        {
+            document.getElementById("update_emoji").innerHTML = "";
+        }
+        if(results[1].label == "Peace Sign")
+        {
+            document.getElementById("update_emoji").innerHTML = "";
+        }
+    }
 }
